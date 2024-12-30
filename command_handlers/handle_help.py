f88f66e8361debddf0919f6e3dc716cc0862f474
Александр Бобин <dembinsky@yandex.ru>
2024-12-30 19:00:20 +0300

    refactor: consolidate config values and help text
    - Move HELP_TEXT constant from help_text.py to config.py
    - Update HELP_TEXT import in handle_help.py
    - Centralize configuration values in config.py

--- command_handlers/handle_help.py
+++ command_handlers/handle_help.py
@@ -1,13 +1,12 @@
 # Обработчик команды /help
 from bot_instance import bot
-from config import DEFAULT_COMMANDS
+from config import DEFAULT_COMMANDS, HELP_TEXT
 
 
 # Обработчик команды /help
 @bot.message_handler(commands=['help'])
 def handle_help(message):
     """Обработчик команды /help"""
-    from help_text import HELP_TEXT
     help_with_commands_description = HELP_TEXT  # Добавляем в вывод текста справки собственно текст без описаний команд
     for command, description in DEFAULT_COMMANDS:  # Добавляем в вывод текста справки описания каждой команды:
         help_with_commands_description = f"{help_with_commands_description}/{command} - {description}\n"
